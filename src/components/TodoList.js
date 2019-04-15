ac4e09a2f28fff06e136e33013b3f207d4026c09
Irfan Suleman <ivsuleman@gmail.com>
2019-04-15 16:58:46 +0100

    added functionality to strike-through completed todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,31 +1,56 @@
 import React, { Component } from "react";
 
-const TodoList = ({ todos, deleteTodo }) => {
-  const todoList = todos.length ? (
-    todos.map(todo => {
-      return (
-        <div className="collection-item" key={todo.id}>
-          <span onClick={() => deleteTodo(todo.id)}>
-            <a className="btn-floating btn-small waves-effect waves-light red">
-              <i className="material-icons">delete_forever</i>
-            </a>
-          </span>
-          <span>
-            <a className="btn-floating btn-small waves-effect waves-light green">
-              <i className="material-icons">check</i>
-            </a>
-          </span>
-          <span>{todo.date}</span>
-          <span>{` ${todo.name} | `}</span>
-          <span>{todo.description}</span>
-        </div>
-      );
-    })
-  ) : (
-    <p className="center">You have No ToDos left, Boom!</p>
-  );
+class TodoList extends Component {
+  render() {
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#cdcdcd",
+      textDecoration: "line-through"
+    };
 
-  return <div className="todoList collection">{todoList}</div>;
-};
+    const todoList = this.props.todos.length ? (
+      this.props.todos.map(todo => {
+        return (
+          <div className="collection-item" key={todo.id}>
+            <span onClick={() => this.props.deleteTodo(todo.id)}>
+              <a className="btn-floating btn-small waves-effect waves-light red">
+                <i className="material-icons">delete_forever</i>
+              </a>
+            </span>
+            <span>
+              <label>
+                <input
+                  type="checkbox"
+                  className="filled-in"
+                  checked={todo.completed}
+                  onChange={() => this.props.toggleTodo(todo.id)}
+                />
+                <span />
+              </label>
+            </span>
+            <span>{todo.date}</span>
+            {todo.editMode ? (
+              <div>
+                <input type="text" defaultValue={todo.name} />
+                <input type="text" defaultValue={todo.description} />
+              </div>
+            ) : (
+              <div
+                onDoubleClick={() => this.props.editTodo(todo.id)}
+                style={todo.completed ? completedStyle : null}
+              >
+                <span>{` ${todo.name} | `}</span>
+                <span>{todo.description}</span>
+              </div>
+            )}
+          </div>
+        );
+      })
+    ) : (
+      <p className="center">You have No ToDos left, Boom!</p>
+    );
+    return <div className="todoList collection">{todoList}</div>;
+  }
+}
 
 export default TodoList;
--- src/containers/App.js
+++ src/containers/App.js
@@ -12,14 +12,16 @@ class App extends Component {
         date: "01/01/2001",
         name: "milk",
         description: "buy 1 gallon full fat milk",
-        completed: false
+        completed: true,
+        editMode: false
       },
       {
         id: 2,
         date: "02/02/2002",
         name: "football",
         description: "watch lfc vs chelsea on sunday 2pm",
-        completed: false
+        completed: false,
+        editMode: false
       }
     ]
   };
@@ -38,11 +40,47 @@ class App extends Component {
     });
   };
 
+  toggleTodo = id => {
+    let todos = [...this.state.todos];
+
+    todos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+
+    console.log("clicked toggle", id);
+
+    this.setState({
+      todos
+    });
+  };
+
   addTodo = todo => {
     todo.id = this.generateUniqueId();
     todo.date = new Date().toLocaleDateString("en-GB");
     todo.completed = false;
+    todo.editMode = false;
     let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    });
+  };
+
+  editTodo = id => {
+    let todos = [...this.state.todos];
+
+    todos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.editMode = true;
+        todo.completed = false;
+      }
+      return todo;
+    });
+
+    console.log(todos);
+
     this.setState({
       todos
     });
@@ -52,7 +90,12 @@ class App extends Component {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">ToDo List with Auditing</h1>
-        <TodoList todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <TodoList
+          todos={this.state.todos}
+          deleteTodo={this.deleteTodo}
+          toggleTodo={this.toggleTodo}
+          editTodo={this.editTodo}
+        />
         <Todoform addTodo={this.addTodo} />
       </div>
     );
